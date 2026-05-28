08ced6c4c3e59627e28f347a34a44cd4f5bb9ddf
perandowind <perandowind@gmail.com>
2026-05-28 11:38:18 +0900

    work

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,10 @@ dependencies {
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
+
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("com.github.codemonstur:embedded-redis:1.4.3")
+
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
     testImplementation("org.springframework.boot:spring-boot-starter-data-redis-test")
     testImplementation("org.springframework.boot:spring-boot-starter-session-data-redis-test")
